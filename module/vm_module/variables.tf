87fdcc207f1da887d8d3563642f7add0c39b5fd6
Ali Yesil <aliyesiluk@yahoo.com>
2023-02-22 22:10:43 +0000

    project done

---
+++ module/vm_module/variables.tf
@@ -0,0 +1,15 @@
+variable "vm_name" {
+}
+variable "resource_group_name" {
+}
+variable "location" {
+default = "East US"
+}
+variable "username" {
+}
+variable "password" {
+}
+variable "subnet_id" {
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "rg_name" {
+    default= "rgali" 
+}
+variable "location" {
+  default = "East Us"
+}
+variable "vnet_name" {
+  default = "vnetali"
+}
+variable "subnet1_name" {
+    default ="subnetali"
+}
+variable "subnet2_name" {
+    default ="subnetyesil"
+}
+variable "rg2_name" {
+    default= "rgali2" 
+}
